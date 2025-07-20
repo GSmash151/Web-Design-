561254af08b8d6ecc2c288ec77895008fb5594c5
GSmash151 <ganilsmash@gmail.com>
2025-07-20 11:37:32 -0400

    Delete Items

--- Market_Place/puddle/item/views.py
+++ Market_Place/puddle/item/views.py
@@ -35,3 +35,8 @@ def new(request):
         form = NewItemForm
 
     return render(request, "item/form.html", {"form": form, "title": "New item"})
+
+@login_required
+def delete(request, pk):
+  item = get_object_or_404(Item, pk=pk, created_by=request.user)
+  return redirect('dashboard:index')
