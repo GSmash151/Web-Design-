07b79a8cb5365b239ad5f66ab145ca45c2bbdacd
GSmash151 <ganilsmash@gmail.com>
2025-07-20 11:05:13 -0400

    Dashboard section

--- Market_Place/puddle/item/forms.py
+++ Market_Place/puddle/item/forms.py
@@ -1,30 +1,25 @@
 from dataclasses import fields
 from tkinter import Widget
-from django import forms 
+from django import forms
 from .models import Item
 
-INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+INPUT_CLASSES = "w-full py-4 px-6 rounded-xl border"
+
 
 class NewItemForm(forms.ModelForm):
-  class Meta:
-    model = Item
-    fields = ('category', 'name', 'description', 'price', 'image',)
-    Widgets = {
-      'category': forms.Select(attrs={
-        'class': INPUT_CLASSES  
-      }),
-      'name': forms.TextInput(attrs={
-        'class': INPUT_CLASSES  
-      }),
-      'description': forms.Textarea(attrs={
-        'class': INPUT_CLASSES  
-      }),
-      'price': forms.TextInput(attrs={
-        'class': INPUT_CLASSES  
-      }),
-      'image': forms.FileInput(attrs={
-        'class': INPUT_CLASSES  
-      }),
-      
-      
-    }
+    class Meta:
+        model = Item
+        fields = (
+            "category",
+            "name",
+            "description",
+            "price",
+            "image",
+        )
+        Widgets = {
+            "category": forms.Select(attrs={"class": INPUT_CLASSES}),
+            "name": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "description": forms.Textarea(attrs={"class": INPUT_CLASSES}),
+            "price": forms.TextInput(attrs={"class": INPUT_CLASSES}),
+            "image": forms.FileInput(attrs={"class": INPUT_CLASSES}),
+        }
--- Market_Place/puddle/puddle/settings.py
+++ Market_Place/puddle/puddle/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     # My apps
     'core',
     'item',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
